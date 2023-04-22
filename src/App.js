d38edc035a0a9abd34cc222def64f1f69abc78de
shivam-sharma0 <vashistshivam931@gmail.com>
2023-04-22 12:03:36 +0530

    First

--- src/App.js
+++ src/App.js
@@ -5,7 +5,56 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+
+  const [loading, setLoading]=useState(true);
+  const [tours, setTours]=useState([]);
+
+  const removePlace =(id)=>{
+   const newTours= tours.filter((tour)=> tour.id!=id);
+   setTours(newTours)
+  }
+
+
+  const getData = async()=>{
+     setLoading(true);
+
+     try {
+       const response = await fetch(url);
+       const data = await response.json();
+       setLoading(false);
+       setTours(data);
+     } catch (error) {
+       setLoading(true);
+       console.log("Error")
+     }
+   
+  }
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+
+  if(loading){
+     return (
+       <main>
+         <Loading ></Loading>
+       </main>
+     );
+  }
+if(tours.length===0){
+  return <main>
+    <div className="title">
+      <h2>No tour left</h2>
+       <button className='btn' onClick={getData}>Refersh</button>
+    </div>
+  </main>
+}
+  return (
+    <main>
+      <Tours tours={tours} removePlace={removePlace}></Tours>
+    </main>
+  );
 }
 
 export default App
--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id,name,info,image,price,removePlace}) => {
+  const[readMore,setReadmore]=useState(false);
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt="" />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>{ readMore ?info: info.substring(0,195)
+          }..... 
+          <button onClick={()=>setReadmore(!readMore)}>{readMore ?"Show Less":"Read More"}</button>
+          </p>
+        <button className="delete-btn" onClick={()=>removePlace(id)}>Not Interested</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
